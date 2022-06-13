101e5081e67661dd8ea4e2391742ea0e129caf9a
KatarzynaRuksza <katarzyna.rukszacyf@gmail.com>
2022-06-13 18:40:37 +0100

    level 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import Names from "./Names";
+import babyNamesData from "./data/babyNamesData.json";
 
 const App = () => {
+  const [Value, setValue] = useState(babyNamesData);
+
+  const search = (input) => {
+    const insertedName = input.target.value.toLowerCase();
+    setValue(
+      babyNamesData.filter((name) =>
+        name.name.toLowerCase().includes(insertedName)
+      )
+    );
+  };
   return (
     <div>
-      <Names />
+      <input
+        type={"text"}
+        onKeyUp={search}
+        className="search-box "
+        placeholder="Insert baby name you are looking for here..."
+      />
+      <Names names={Value} />
     </div>
   );
 };
--- src/Names.js
+++ src/Names.js
@@ -1,27 +1,31 @@
 import React from "react";
-import babyNamesData from "./data/babyNamesData.json";
+//import babyNamesData from "./data/babyNamesData.json";
 
-const names = babyNamesData;
+//const names = babyNamesData;
 
-const Names = () => {
+const Names = (props) => {
   return (
-    <div className="App-content container-fluid">
-      <div className="row">
-        {names.map((name) => {
-          if (name.sex === "f") {
-            return (
-              <ul className="female col-xs-2 m-2 badge rounded-pill fa fa-female">
-                {name.name}
-              </ul>
-            );
-          } else {
-            return (
-              <ul className="male col-xs-2 m-2 badge fa fa-male">
-                {name.name}
-              </ul>
-            );
-          }
-        })}
+    <div>
+      <div className="App-content container-fluid">
+        <div className="row">
+          {props.names
+            .sort((a, b) => a.name.localeCompare(b.name))
+            .map((name) => {
+              if (name.sex === "f") {
+                return (
+                  <ul className="female col-xs-2 m-2 badge rounded-pill fa fa-female">
+                    {name.name}
+                  </ul>
+                );
+              } else {
+                return (
+                  <ul className="male col-xs-2 m-2 badge fa fa-male">
+                    {name.name}
+                  </ul>
+                );
+              }
+            })}
+        </div>
       </div>
     </div>
   );
